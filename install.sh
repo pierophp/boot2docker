41513a0a4a8cae78bd95b306860dabcf863f45b2
Piero <pierophp@gmail.com>
2015-11-04 17:35:05 -0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -20,11 +20,11 @@ rm -Rf /home/docker
 ln -s  /mnt/sda1/home/docker /home/docker
 
 #Docker Compose
-echo "Instalando Docker Compose"
-sudo mkdir /mnt/sda1/bin
-wget https://github.com/docker/compose/releases/download/1.4.2/docker-compose-`uname -s`-`uname -m` -O /home/docker/docker-compose
-sudo mv /home/docker/docker-compose /mnt/sda1/bin/docker-compose
-sudo chmod +x /mnt/sda1/bin/docker-compose
+#echo "Instalando Docker Compose"
+#sudo mkdir /mnt/sda1/bin
+#wget https://github.com/docker/compose/releases/download/1.4.2/docker-compose-`uname -s`-`uname -m` -O /home/docker/docker-compose
+#sudo mv /home/docker/docker-compose /mnt/sda1/bin/docker-compose
+#sudo chmod +x /mnt/sda1/bin/docker-compose
 
 #baixa o script
 echo "Baixando bootlocal.sh"
